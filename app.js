efa20e635fb873a4e09c00d370357b198ca480dc
Seb Collis <seb.collis@aurecongroup.com>
2024-04-12 15:54:21 +1200

    Beginning setup

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-'use strict';
+import { Viewer, DefaultViewerParams, SpeckleLoader } from "@speckle/viewer";
+import { CameraController } from "@speckle/viewer";
 
-console.log('Hello world');
+async function main() {
+    /** Get the HTML container */
+    const container = document.getElementById("renderer");
+
+    /** Create Viewer instance */
+    const viewer = new Viewer(container, DefaultViewerParams);
+    /** Initialise the viewer */
+    await viewer.init();
+
+    /** Add the stock camera controller extension */
+    viewer.createExtension(CameraController);
+
+    /** Create a loader for the speckle stream */
+    const loader = new SpeckleLoader(
+        viewer.getWorldTree(),
+        "https://latest.speckle.dev/streams/92b620fb17/objects/801360a35cd00c13ac81522851a13341",
+        ""
+    );
+    /** Load the speckle data */
+    await viewer.loadObject(loader, 1, true);
+}
+
+/** Call our function, which we named 'main' */
+main();
